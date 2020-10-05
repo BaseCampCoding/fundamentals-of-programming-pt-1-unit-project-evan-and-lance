543ffcaaefd52c3dbc64c2e6488f0d43ed5f0282
Lance Easley <leasley21@basecampcodingacademy.org>
2020-10-05 15:46:08 -0500

    Updated Customer_generation

--- Customer_generation.py
+++ Customer_generation.py
@@ -1,3 +1,13 @@
 import random
 
 food_choices = ['hamburger', 'cheeseburger', 'fries', 'water']
+order = []
+
+while len(order) < 0:
+    for item in food_choices:
+        choice = random.randint(0,2)
+        if choice == 1:
+            order.append(item)
+
+if __name__ == "__main__":
+    print(order)
