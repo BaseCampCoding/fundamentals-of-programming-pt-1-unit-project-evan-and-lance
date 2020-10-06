5d56ec8aa75440c7060f58e79a715ca1a2d12287
Lance Easley <leasley21@basecampcodingacademy.org>
2020-10-06 10:18:30 -0500

    Turned customer_generation into callable function

--- Customer_generation.py
+++ Customer_generation.py
@@ -1,23 +1,27 @@
 import random
 
-primary_food = ['hamburger', 'cheeseburger', 'chicken nuggets']
-secondary_food = ['fries']
-drinks = ['water']
+def customer():
+    primary_food = ['hamburger', 'cheeseburger', 'chicken nuggets']
+    secondary_food = ['fries']
+    drinks = ['water']
 
-order = []
+    order = []
 
-while len(order) < 1:
-    choice = random.randint(0, len(primary_food))
-    if choice != 0:
-        order.append(primary_food[choice - 1])
+    while len(order) < 1:
+        choice = random.randint(0, len(primary_food))
+        if choice != 0:
+            order.append(primary_food[choice - 1])
 
-    choice = random.randint(0, len(secondary_food))
-    if choice != 0:
-        order.append(secondary_food[choice - 1])
+        choice = random.randint(0, len(secondary_food))
+        if choice != 0:
+            order.append(secondary_food[choice - 1])
 
-    choice = random.randint(0, len(drinks))
-    if choice != 0:
-        order.append(drinks[choice - 1])
+        choice = random.randint(0, len(drinks))
+        if choice != 0:
+            order.append(drinks[choice - 1])
+    return order
+
+order = customer()
 
 if __name__ == "__main__":
     print(order)
