ba0a599df936d638c43a0631ddcc405da6115dd0
Evan Sanders <esanders21@basecampcodingacademy.org>
2020-10-06 10:20:37 -0500

    Extras

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 # main file to run
+ 
+
+name = input("What is your name?: ")
+
+print("Welcome Chef", name.strip(), "What would you like to cook today?")
+
+import Customer_generation 
+# while True: 
+#     cuisine_type = input("Breakfast, lunch, or dinner?: ")
+#     if cuisine_type.upper() == 'BREAKFAST':
+#      input("The breakfast menu for today includes...\nPancakes \nWaffles \nFrench Toast \nWhich would you like to cook Chef " + name + "? ")
+#      break
+#     if cuisine_type.upper() == 'LUNCH':
+#      input("The lunch menu for today includes... \nBurger \nBBQ Ribs \nFries \nWhich would you like to cook Chef " + name + "? ")
+#      break
+#     if cuisine_type.upper() == 'DINNER':
+#      input("The dinner menu for today includes...\nSteak \nLobster \nFettuccine alfredo. \nWhich would you like to cook Chef " + name + "? ")
+#      break
+
+# difficulty = input("(Easy or Hard?) ")
+# if difficulty == 'Easy' or 'easy':
+#     print("You come into work, and you feel today is going to be easy as pie!")
+# elif difficulty == 'Hard' or 'hard':
+#     print("You come into work, and you feel today is going to be hard day!")
