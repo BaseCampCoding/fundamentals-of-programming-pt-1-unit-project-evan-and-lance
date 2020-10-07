b29e3a5ae08329c8b21feaf6ced276989a5e6763
Lance Easley <leasley21@basecampcodingacademy.org>
2020-10-07 09:35:18 -0500

    Updated main.py for updated lunch_customer_gen name

--- main.py
+++ main.py
@@ -5,24 +5,7 @@ name = input("What is your name?: ")
 
 print("Welcome Chef", name.strip(), "Looks like there's a customer at the desk. Lets see what he wants.")
 
-import Customer_generation
+import lunch_customer_gen
 
-order = str(Customer_generation.customer())[1:-1]
+order = str(lunch_customer_gen.customer())[1:-1]
 print("Their order is written on this list: " + order.replace("'", ''))
-# while True: 
-#     cuisine_type = input("Breakfast, lunch, or dinner?: ")
-#     if cuisine_type.upper() == 'BREAKFAST':
-#      input("The breakfast menu for today includes...\nPancakes \nWaffles \nFrench Toast \nWhich would you like to cook Chef " + name + "? ")
-#      break
-#     if cuisine_type.upper() == 'LUNCH':
-#      input("The lunch menu for today includes... \nBurger \nBBQ Ribs \nFries \nWhich would you like to cook Chef " + name + "? ")
-#      break
-#     if cuisine_type.upper() == 'DINNER':
-#      input("The dinner menu for today includes...\nSteak \nLobster \nFettuccine alfredo. \nWhich would you like to cook Chef " + name + "? ")
-#      break
-
-# difficulty = input("(Easy or Hard?) ")
-# if difficulty == 'Easy' or 'easy':
-#     print("You come into work, and you feel today is going to be easy as pie!")
-# elif difficulty == 'Hard' or 'hard':
-#     print("You come into work, and you feel today is going to be hard day!")
