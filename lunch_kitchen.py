2c2bc484b2827f16a5ef758c636d0b6c327a7b25
Lance Easley <leasley21@basecampcodingacademy.org>
2020-10-07 11:15:20 -0500

    Finished lunch_kitchen and added it to main.py. Game is now playable and winnable

--- lunch_kitchen.py
+++ lunch_kitchen.py
@@ -15,132 +15,138 @@ import time
 # print(elapsed)
 # input()
 
-prepared_food = []
-while True:
-    print("""Stations:
-- Burger Station
-- Frier Station
-- Drink Station
-    """)
-    if len(prepared_food) > 0:
-        print('You have prepared:')
-        for i in prepared_food:
+def kitchen(order: list) -> list:
+    prepared_food = []
+    while True:
+        print("\nCustomer's order:")
+        for i in order:
             print(f'- {i}')
-    station_choice = input("Which station do you need? (Type 'c' to confirm order): ").lower()
-    if len(station_choice) > 0:
-        if station_choice[0] == 'b':
-            # Burger station
-            print('You are at the Burger Station')
-            cooked = False
-            progress = 0
-            while True and progress < 2:
-                choice = input('Grill or Assembly? ')
-                choice = choice .lower()
-                if len(choice) > 0:
-                    if choice[0] == 'g':
-                        if not cooked:
-                            input('Press Enter to start grilling')
-                            print('Pattie is cooking...')
-                            while True:
-                                time.sleep(1)
-                                input('Press Enter to flip pattie')
+        print("""Stations:
+    - Burger Station
+    - Frier Station
+    - Drink Station
+        """)
+        if len(prepared_food) > 0:
+            print('You have prepared:')
+            for i in prepared_food:
+                print(f'- {i}')
+        station_choice = input("Which station do you need? (Type 'c' to confirm order): ").lower()
+        if len(station_choice) > 0:
+            if station_choice[0] == 'b':
+                # Burger station
+                print('You are at the Burger Station')
+                cooked = False
+                progress = 0
+                while True and progress < 2:
+                    choice = input('Grill or Assembly? ')
+                    choice = choice .lower()
+                    if len(choice) > 0:
+                        if choice[0] == 'g':
+                            if not cooked:
+                                input('Press Enter to start grilling')
                                 print('Pattie is cooking...')
-                                time.sleep(1)
-                                break
-                            print('Pattie is done')
-                            time.sleep(0.25)
-                            cooked = True
-                            progress += 1
-                        else:
-                            print('Pattie is already cooked')
-                    elif choice[0] == 'a' and cooked:
-                        ingredients = [
-                            'top bun', 'mayonaise', 'onion', 'lettuce', 'tomato', 
-                            'cheese', 'pattie', 'pickles', 'mustard', 'bottom bun'
-                        ]
-                        burger = []
-                        print('Burger Ingredients:')
-                        for i in ingredients:
-                            print(f'- {i}')
-                            time.sleep(0.05)
-                        time.sleep(0.25)
-                        print('Assemble burger: (Type ingredients to add)')
-                        print("Add ingredient or type 'q' to quit")
-                        while True:
-                            addition = input()
-                            if len(addition) > 0:
-                                if addition != 'q':
-                                    burger.append(addition)
-                                else:
-                                    progress += 1
+                                while True:
+                                    time.sleep(10)
+                                    input('Press Enter to flip pattie')
+                                    print('Pattie is cooking...')
+                                    time.sleep(10)
                                     break
+                                print('Pattie is done')
+                                time.sleep(0.25)
+                                cooked = True
+                                progress += 1
                             else:
-                                print('Please choose a valid ingredient')
-                        complete = False
-                        progress = 0
-                        for i in ingredients:
-                            if i != 'cheese':
-                                if i in burger:
-                                    progress += 1
+                                print('Pattie is already cooked')
+                        elif choice[0] == 'a' and cooked:
+                            ingredients = [
+                                'top bun', 'mayonaise', 'onion', 'lettuce', 'tomato', 
+                                'cheese', 'pattie', 'pickles', 'mustard', 'bottom bun'
+                            ]
+                            burger = []
+                            print('Burger Ingredients:')
+                            for i in ingredients:
+                                print(f'- {i}')
+                                time.sleep(0.05)
+                            time.sleep(0.25)
+                            print('Assemble burger: (Type ingredients to add)')
+                            print("Add ingredient or type 'c' to confirm")
+                            while True:
+                                addition = input()
+                                if len(addition) > 0:
+                                    if addition != 'c':
+                                        burger.append(addition)
+                                    else:
+                                        progress += 1
+                                        break
                                 else:
-                                    print(f'You forgot the {i}!')
-                        if progress >= 9:
-                            complete = True
-                        if 'cheese' in burger and complete:
-                                prepared_food.append('cheeseburger')
-                        elif 'cheese' not in burger and complete:
-                                prepared_food.append('hamburger')
-                    else:
-                        print('Make sure to have a pattie cooked first!')
-                        time.sleep(0.75)
+                                    print('Please choose a valid ingredient')
+                            complete = False
+                            progress = 0
+                            for i in ingredients:
+                                if i != 'cheese':
+                                    if i in burger:
+                                        progress += 1
+                                    else:
+                                        print(f'You forgot the {i}!')
+                            if progress >= 9:
+                                complete = True
+                            if 'cheese' in burger and complete:
+                                    prepared_food.append('1 cheeseburger')
+                            elif 'cheese' not in burger and complete:
+                                    prepared_food.append('1 hamburger')
+                        else:
+                            print('Make sure to have a pattie cooked first!')
+                            time.sleep(0.75)
 
-        if station_choice[0] == 'f':
-            # Frier station
-            print('You are at the Frier Station')
-            fried = False
-            while True and not fried:
-                items = ['fries', 'chicken nuggets']
-                print('Foods to fry:')
-                for i in items:
-                    print(f'- {i}')
-                    time.sleep(0.05)
-                food_choice = input('What do you need to fry? ').lower()
-                if food_choice in items:
-                    print(f'Frying {food_choice}...')
-                    time.sleep(15)
-                    print('Done!')
-                    time.sleep(0.5)
-                    fried = True
-                    prepared_food.append(food_choice)
-                    break
-                else:
-                    print("We don't have that")
-                    time.sleep(0.75)
-
-        if station_choice[0] == 'd':
-            # Drink Station ###############################################
-            print('You are at the Drink Station')
-            filled = False
-            while True: 
-                drink_choice = input("Sweet Tea or Water? ").lower()
-                if drink_choice == 'sweet tea' or drink_choice == 'water':
-                    if not filled:
-                        input("Press enter to fill cup")
-                        print("Cup is filling")
-                        while True:
-                            time.sleep(3)
-                            input('Press enter to put top on cup')
-                            print('Cup is being prepared...')
-                            time.sleep(3)
-                            break
-                        print('Drink is done')
-                        time.sleep(0.75)
-                        filled = True
-                        prepared_food.append(drink_choice)
+            if station_choice[0] == 'f':
+                # Frier station
+                print('You are at the Frier Station')
+                fried = False
+                while True and not fried:
+                    items = ['fries', 'chicken nuggets']
+                    print('Foods to fry:')
+                    for i in items:
+                        print(f'- {i}')
+                        time.sleep(0.05)
+                    food_choice = input('What do you need to fry? ').lower()
+                    if food_choice in items:
+                        print(f'Frying {food_choice}...')
+                        time.sleep(15)
+                        print('Done!')
+                        time.sleep(0.5)
+                        fried = True
+                        prepared_food.append(f'1 order of {food_choice}')
                         break
                     else:
-                        print("Drink is already prepared")
-                else: 
-                    print("Make sure we have that drink first!")
-    else:
-        print("Please choose one of the provided stations")
+                        print("We don't have that")
+                        time.sleep(0.75)
+
+            if station_choice[0] == 'd':
+                # Drink Station ###############################################
+                print('You are at the Drink Station')
+                filled = False
+                while True: 
+                    drink_choice = input("Sweet Tea or Water? ").lower()
+                    if drink_choice == 'sweet tea' or drink_choice == 'water':
+                        if not filled:
+                            input("Press enter to fill cup")
+                            print("Cup is filling")
+                            while True:
+                                time.sleep(3)
+                                input('Press enter to put top on cup')
+                                print('Cup is being prepared...')
+                                time.sleep(3)
+                                break
+                            print('Drink is done')
+                            time.sleep(0.75)
+                            filled = True
+                            prepared_food.append(f'1 {drink_choice}')
+                            break
+                        else:
+                            print("Drink is already prepared")
+                    else: 
+                        print("Make sure we have that drink first!")
+            if station_choice[0] == 'c':
+                return prepared_food
+        else:
+            print("Please choose one of the provided stations")
--- main.py
+++ main.py
@@ -1,11 +1,86 @@
 # main file to run
- 
+
+import lunch_customer_gen
+import lunch_kitchen
+import time
+
+def confirm_food(food: list) -> bool:
+    while True:
+        confirm = input('Are you ready to give this to the customer? ')
+        if len(confirm) > 0:
+            if confirm[0] == 'y':
+                return True
+            elif confirm[0] == 'n':
+                return False
+            else:
+                print('Please type yes or no') 
+        else:
+            print('Please type yes or no')
 
 name = input("What is your name?: ")
 
-print("Welcome Chef", name.strip(), "Looks like there's a customer at the desk. Lets see what he wants.")
+print(f'Welcome Chef {name.strip()},')
 
-import lunch_customer_gen
+while True: 
+    cuisine_type = input("Breakfast, lunch, or dinner?: ").lower()
+    if len(cuisine_type) > 0:
+        if cuisine_type[0] == 'b':
+            pass
+        elif cuisine_type[0] == 'l':
+            # Lunch
+            order = lunch_customer_gen.customer()
+            print("Here's a customer! Their order is written on this list: ")
+            for i in order:
+                print(f'- {i}')
+            input("Press Enter to start cooking ")
+            begin = time.time()
+            food = lunch_kitchen.kitchen(order)
+            if confirm_food(food):
+                end = time.time()
+                break
+        elif cuisine_type[0] == 'd':
+            pass
 
-order = str(lunch_customer_gen.customer())[1:-1]
-print("Their order is written on this list: " + order.replace("'", ''))
+elapsed = int(end - begin)
+
+if True:
+    print('---dev_info---')
+    print(order)
+    print(food)
+    print(elapsed)
+    print(60 + (60 * len(order)))
+    print('--------------')
+
+time.sleep(0.75)
+
+confirmation = 0
+for i in food:
+    if i in order:
+        confirmation += 1
+if len(order) == confirmation:
+    correct_food = 'The customer got his food'
+else:
+    correct_food = 'The customer did not get his food'
+if elapsed < 60 + (60 * len(order)):
+    in_time = 'it was in time'
+else:
+    in_time = 'it was not in time'
+
+win = False
+if correct_food == 'The customer did not get his food' and in_time == 'it was not in time':
+    between_word = 'and'
+elif correct_food == 'The customer got his food' and in_time == 'it was in time':
+    between_word = 'and'
+    win = True
+else:
+    between_word = 'but'
+
+print(correct_food, between_word, in_time)
+if win:
+    print('You Win!')
+else:
+    print('You Lose.')
+print('')
+
+# order = str(lunch_customer_gen.customer())[1:-1]
+# print("Their order is written on this list: " + order.replace("'", ''))
