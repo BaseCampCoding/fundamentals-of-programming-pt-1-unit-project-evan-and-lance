f75fb7b658fca205278b5b84bed9ab8d81db262a
Lance Easley <leasley21@basecampcodingacademy.org>
2020-10-08 10:21:36 -0500

    Revised Burger Station: There was no reason to choose between grill and assembly. The user had to cook the pattie first anyway.

--- lunch_kitchen.py
+++ lunch_kitchen.py
@@ -27,68 +27,52 @@ def kitchen(order: list) -> list:
             if station_choice[0] == 'b':
     # Burger Station
                 print('You are at the Burger Station')
-                cooked = False
-                progress = 0
-                while True and progress < 2:
-                    choice = input('Grill or Assembly? ')
-                    choice = choice .lower()
-                    if len(choice) > 0:
-                        if choice[0] == 'g':
-                            if not cooked:
-                                input('Press Enter to start grilling')
-                                print('Pattie is cooking...')
-                                while True:
-                                    time.sleep(10)
-                                    input('Press Enter to flip pattie')
-                                    print('Pattie is cooking...')
-                                    time.sleep(10)
-                                    break
-                                print('Pattie is done')
-                                time.sleep(0.25)
-                                cooked = True
-                                progress += 1
-                            else:
-                                print('Pattie is already cooked')
-                        elif choice[0] == 'a' and cooked:
-                            ingredients = [
-                                'top bun', 'mayonaise', 'onion', 'lettuce', 'tomato', 
-                                'cheese', 'pattie', 'pickles', 'mustard', 'bottom bun'
-                            ]
-                            burger = []
-                            print('Burger Ingredients:')
-                            for i in ingredients:
-                                print(f'- {i}')
-                                time.sleep(0.05)
-                            time.sleep(0.25)
-                            print('Assemble burger: (Type ingredients to add)')
-                            print("Add ingredient or type 'c' to confirm")
-                            while True:
-                                addition = input()
-                                if len(addition) > 0:
-                                    if addition != 'c':
-                                        burger.append(addition)
-                                    else:
-                                        progress += 1
-                                        break
-                                else:
-                                    print('Please choose a valid ingredient')
-                            complete = False
-                            progress = 0
-                            for i in ingredients:
-                                if i != 'cheese':
-                                    if i in burger:
-                                        progress += 1
-                                    else:
-                                        print(f'You forgot the {i}!')
-                            if progress >= 9:
-                                complete = True
-                            if 'cheese' in burger and complete:
-                                    prepared_food.append('1 cheeseburger')
-                            elif 'cheese' not in burger and complete:
-                                    prepared_food.append('1 hamburger')
+                time.sleep(0.5)
+                input('Press Enter to start grilling pattie')
+                print('Pattie is cooking...')
+                time.sleep(10)
+                input('Press Enter to flip pattie')
+                print('Pattie is cooking...')
+                time.sleep(10)
+                print('Pattie is done')
+                time.sleep(0.25)
+                ingredients = [
+                    'top bun', 'mayonaise', 'onion', 'lettuce', 'tomato', 
+                    'cheese', 'pattie', 'pickles', 'mustard', 'bottom bun'
+                ]
+                burger = []
+                print('Burger Ingredients:')
+                for i in ingredients:
+                    print(f'- {i}')
+                    time.sleep(0.05)
+                time.sleep(0.25)
+                print('Assemble burger: (Type ingredients to add)')
+                print("Add ingredient or type 'c' to confirm")
+                while True:
+                    addition = input()
+                    if len(addition) > 0:
+                        if addition != 'c':
+                            burger.append(addition)
                         else:
-                            print('Make sure to have a pattie cooked first!')
-                            time.sleep(0.75)
+                            break
+                    else:
+                        print('Please choose a valid ingredient')
+                progress = 0
+                for i in ingredients:
+                    if i != 'cheese':
+                        if i in burger:
+                            progress += 1
+                        else:
+                            print(f'You forgot the {i}!')
+                if progress >= 9:
+                    complete = True
+                if 'cheese' in burger and complete:
+                    prepared_food.append('1 cheeseburger')
+                elif 'cheese' not in burger and complete:
+                    prepared_food.append('1 hamburger')
+                else:
+                    print('Make sure to have a pattie cooked first!')
+                    time.sleep(0.75)
 
             if station_choice[0] == 'f':
     # Frier Station
